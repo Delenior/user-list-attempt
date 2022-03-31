44456415280c227af394f034d88c42367a0f147f
Walter Fülöp <delenior001@gmail.com>
2022-03-31 10:51:02 +0200

    Commenting end cleaning service file

--- src/app/core/services/user.service.ts
+++ src/app/core/services/user.service.ts
@@ -7,17 +7,16 @@ import { Observable } from 'rxjs';
 })
 export class UserService {
 
+  // Ez fogja eltárolni az editálni kívánt usert
   editingUser: any;
   
+  // Ez a változó tárolja globálisan a usereket
   allUsers: any[] = [];
   
   constructor(private http: HttpClient) { }
 
+  // Http kérés a szerver felé
   getUsers(): Observable<any> {
     return this.http.get('https://assessment-users-backend.herokuapp.com/users.json');
   }
-
- /* addUser(body: any): Observable<any> {
-    return this.http.post('https://assessment-users-backend.herokuapp.com/users', body);
-  }*/
 }
